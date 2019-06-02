babeab847350271a960eb4bbea51d3b73ebf39cf
ashitani0803 <ashitani0803@gmail.com>
2019-06-02 18:06:43 +0000

    [Finish] level1-9

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :books do
   	resources :book_comments, only: [:create, :edit, :update, :destroy]
+  	resource :favorites, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
