fee851f02ea10136f64415141153792ec8ab132b
ashitani0803 <ashitani0803@gmail.com>
2019-06-02 14:16:23 +0000

    [Finish] level1

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,19 +5,26 @@ class BookCommentsController < ApplicationController
 		comment = current_user.book_comments.new(book_comment_params)
 		comment.book_id = book.id
 		comment.save
-		redirect_to book_path(@book.id)
+		redirect_to book_path(book.id)
 	end
 
 	def edit
-
+		@book = Book.find(params[:book_id])
+		@comment = @book.book_comments.find(params[:id])
 	end
 
 	def update
-
+		@book = Book.find(params[:book_id])
+		@comment = @book.book_comments.find(params[:id])
+		@comment.update(book_comment_params)
+		redirect_to book_path(@book.id)
 	end
 
 	def destory
-
+		@book = Book.find(params[:book_id])
+		@comment = @book.book_comments.find(params[:id])
+		@comment.destory
+		redirect_to book_path(@book.id)
 	end
 
 	private
